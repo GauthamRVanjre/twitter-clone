2e5fa98f7ecea518a3e3e1e26f827224176b47df
Gautham Vanjre <vanjregautham@gmail.com>
2023-09-10 12:54:30 +0530

    implemented NextAuth authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import users from "@/app/helpers/constants";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", placeholder: "Enter your email" },
+        password: { label: "password", placeholder: "Enter your password" },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.password)
+          return null;
+
+        const user = users.find((item) => item.email === credentials.email);
+        if (user?.password === credentials.password) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ app/helpers/constants.ts
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: "1",
+    name: "gautham",
+    email: "gautham@gmail.com",
+    password: "123",
+  },
+  {
+    id: "2",
+    name: "ghambir",
+    email: "ghambir@gmail.com",
+    password: "123",
+  },
+];
+
+export default users;
