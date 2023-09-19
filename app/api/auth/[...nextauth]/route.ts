0594e1c7cddb255bae7231a8d1397e2bfde2ebf7
Gautham Vanjre <vanjregautham@gmail.com>
2023-09-19 12:15:24 +0530

    refracted nextAuth api to include user id

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -48,6 +48,15 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
 };
 const handler = NextAuth(authOptions);
 
